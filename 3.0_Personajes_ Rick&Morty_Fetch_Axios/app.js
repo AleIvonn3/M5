854507ff62baac8da9218ed23adf025390464ea0
Ale Ivonne <aleivonne.ma@gmail.com>
2025-06-10 21:59:32 -0600

    js3

---
+++ 3.0_Personajes_ Rick&Morty_Fetch_Axios/app.js
@@ -0,0 +1,54 @@
+// Implementa las Solicitudes con Fetch
+const fetchBtn = document.getElementById('fetch-btn');
+const dataContainer = document.getElementById('data-container');
+
+fetchBtn.addEventListener('click', () => {
+  fetch('https://rickandmortyapi.com/api/character')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Error en la solicitud');
+      }
+      return response.json();
+    })
+    .then(data => {
+      renderCharacters(data.results); // Renderiza los personajes usando la función
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      dataContainer.textContent = 'Hubo un error al obtener los datos.';
+    });
+});
+
+// Implementa las Solicitudes con Axios
+const axiosBtn = document.getElementById('axios-btn');
+
+axiosBtn.addEventListener('click', () => {
+  axios.get('https://rickandmortyapi.com/api/character')
+    .then(response => {
+      const data = response.data;
+      renderCharacters(data.results); // Renderiza los personajes usando la función
+    
+    // Cambia el color de los nombres al hacer click en Axios
+      document.querySelectorAll('#data-container h3').forEach(h3 => {
+        h3.classList.add('h3-axios-active');
+      });
+    
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      dataContainer.textContent = 'Hubo un error al obtener los datos.';
+    });
+});
+
+// Función para renderizar los personajes
+function renderCharacters(characters) {
+  dataContainer.innerHTML = '';
+  characters.forEach(character => {
+    const characterElement = document.createElement('div');
+    characterElement.innerHTML = `
+      <h3>${character.name}</h3>
+      <img src="${character.image}" alt="${character.name}">
+    `;
+    dataContainer.appendChild(characterElement);
+  });
+}
