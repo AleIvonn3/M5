a4ba6edaccd99f63dc76ed2104b1027bf99add1d
Ale Ivonne <aleivonne.ma@gmail.com>
2025-06-27 19:43:02 -0600

    js 7

---
+++ 7.0_mi-exploracion-espacial/planetas.js
@@ -0,0 +1,41 @@
+const planetas = [
+  {
+    nombre: "Titán",
+    descripcion: "La luna más grande de Saturno, con lagos de metano.",
+    descubiertoEn: "1655"
+  },
+  {
+    nombre: "Próxima Centauri b",
+    descripcion: "Un exoplaneta rocoso en la zona habitable de su estrella.",
+    descubiertoEn: "2016"
+  },
+  // ¡Añade más planetas!
+
+  {
+    nombre: "Kepler-452b",
+    descripcion: "Un exoplaneta considerado una 'Tierra 2.0' por su tamaño y órbita similar.",
+    descubiertoEn: "2015"
+  },
+  {
+    nombre: "Europa",
+    descripcion: "Una luna de Júpiter con un océano subterráneo de agua líquida.",
+    descubiertoEn: "1610"
+  },
+  {
+    nombre: "Gliese 581g",
+    descripcion: "Un posible exoplaneta en la zona habitable de la estrella Gliese 581.",
+    descubiertoEn: "2010"
+  },
+  {
+    nombre: "Encelado",
+    descripcion: "Luna de Saturno con géiseres de agua que sugieren un océano interno.",
+    descubiertoEn: "1789"
+  },
+  {
+    nombre: "TRAPPIST-1e",
+    descripcion: "Uno de los siete exoplanetas rocosos que orbitan la estrella TRAPPIST-1.",
+    descubiertoEn: "2017"
+  }
+];
+
+module.exports = planetas;
